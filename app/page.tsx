65076bc32f3fd8250be68a317b0314ded14b9f85
Aryagoy <aryagoyal1301@gmail.com>
2024-08-01 00:51:54 +0530

    add register form and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,27 @@
 import { LoginForm } from "@/pages/LoginForm";
 import Image from "next/image";
+import Link from "next/link";
 // Design the login page first
-//left form , right image
+//left image , right form
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
+      <Image
+        src="/assets/images/loginImage.jpg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
       <section className="remove-scrollbar container my-auto">
-        <Image
-          src="/assets/icons/download.png"
-          alt="patient"
-          width={50}
-          height={50}
-          className="mb-12 h-10 w-fit"
-        />
         <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/download.png"
+            alt="patient"
+            width={50}
+            height={50}
+            className="mb-12 h-10 w-fit"
+          />
           {/* TODO : otp verification */}
           <LoginForm />
         </div>
---
+++ app/patients/[userId]/register/page.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+
+const Register = () => {
+  return (
+    <div className="flex h-screen max-h-screen">
+      <Image
+        src="/assets/images/registerImage.jpg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[390px]"
+      />
+      <section className="remove-scrollbar container">
+      <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
+          <Image
+            src="/assets/icons/download.png"
+            alt="patient"
+            width={50}
+            height={50}
+            className="mb-12 h-10 w-fit"
+          />
+          {/* <RegisterForm /> */}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Register;
